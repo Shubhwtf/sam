9820ddc93b9b4c9c5cd46135d5300c47727ba75f
Shubhwtf <lolgaming123yt@gmail.com>
2024-10-13 22:13:00 +0530

    added files

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('give-chance').addEventListener('click', function() {
+    alert("Thank you for giving me another chance. I won't rush it this time. 💙");
+});
+
+document.getElementById('stay-friends').addEventListener('click', function() {
+    alert("I understand. I value our friendship and will always respect that. 💔");
+});
